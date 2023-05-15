6f1e4102e7b096ce3dbc6c7162ee7e9f121607a9
Dan Caron <dancaron@gmail.com>
2023-05-15 18:19:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ st.title('Gupta Postoperative Respiratory Failure Risk Calculator')
 st.write('This application is intended for medical professionals to calculate the Gupta Postoperative Respiratory Failure Risk. This score helps in predicting the risk of postoperative respiratory failure.')
 
 age = st.slider('Age', 0, 100, 30)
-functional_status = st.selectbox('Functional Status', ['Independent', 'Partially Dependent', 'Totally Dependent'])
-ascites = st.selectbox('Ascites', ['No', 'Yes'])
-copd = st.selectbox('Chronic Obstructive Pulmonary Disease (COPD)', ['No', 'Yes'])
-sepsis = st.selectbox('Systemic Sepsis', ['No', 'Yes'])
-surgery_type = st.selectbox('Type of Surgery', ['Intraperitoneal', 'Intrathoracic', 'Aortic', 'Peripheral Vascular'])
+functional_status = st.selectbox('Functional Status', [(0, 'Independent'), (1, 'Partially Dependent'), (2, 'Totally Dependent')])
+ascites = st.selectbox('Ascites', [(0, 'No'), (1, 'Yes')])
+copd = st.selectbox('Chronic Obstructive Pulmonary Disease (COPD)', [(0, 'No'), (1, 'Yes')])
+sepsis = st.selectbox('Systemic Sepsis', [(0, 'No'), (1, 'Yes')])
+surgery_type = st.selectbox('Type of Surgery', [(0, 'Intraperitoneal'), (1, 'Intrathoracic'), (2, 'Aortic'), (3, 'Peripheral Vascular')])
 
 if st.button('Calculate Score'):
-    score = calculate_gupta_score(age, functional_status, ascites, copd, sepsis, surgery_type)
+    score = calculate_gupta_score(age, functional_status[0], ascites[0], copd[0], sepsis[0], surgery_type[0])
     st.write('The Gupta Postoperative Respiratory Failure Risk is: ', score)
